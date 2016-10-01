6cefad460825a0781ba367051c6df9c8cc1aa778
NORTHNRORO\Northnroro <northnroro@hotmail.com>
2016-10-01 16:04:45 +0700

    no message

--- RadarWeb/script.js
+++ RadarWeb/script.js
@@ -395,7 +395,7 @@ function redrawMarker(){
 					pokemonList[0][hash] = {time:0};
 				}
 				var count = pokemonList[num][hash].count;
-				if((count/(pokemonList[0][hash].time+1) >= max * 0.2 || rareList[num]) && !filterdict[num]){
+				if((count/(pokemonList[0][hash].time+1) >= max * 0.3 || (rareList[num] && count/(pokemonList[0][hash].time+1) >= max * 0.1)) && !filterdict[num]){
 					var pokeMarker=new L.marker(new L.LatLng(pokemonList[num][hash].lat,pokemonList[num][hash].lng),{icon:createPokeIcon(num,Date.now(),false)});
 					map.addLayer(pokeMarker);
 					pokeMarker.setLatLng(new L.LatLng(pokemonList[num][hash].lat,pokemonList[num][hash].lng));
